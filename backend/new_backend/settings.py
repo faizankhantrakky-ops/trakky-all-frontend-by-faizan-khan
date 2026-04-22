307bb478ed87111a1491346ecc4aa47fe2fce756
devaltrakky <devaltrakky1@gmail.com>
2026-04-22 12:19:38 +0530

    Decrrease Token Life Time

--- backend/new_backend/settings.py
+++ backend/new_backend/settings.py
@@ -143,8 +143,8 @@ SPECTACULAR_SETTINGS = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=10),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=13),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
