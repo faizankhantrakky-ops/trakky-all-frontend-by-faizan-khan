509436f6a86fb96b22ddc21fbde54ffd3f448327
devaltrakky <devaltrakky1@gmail.com>
2026-04-25 12:21:01 +0530

    Fix Structure

--- spas/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SpasConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'spas'
--- spavendor/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SpavendorConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "spavendor"
